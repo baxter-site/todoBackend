2559381cfca7a8884ebce0b73524a05bd6b260be
baxter-site <baxter.site@gmail.com>
2024-09-13 12:22:44 +0300

    pert2

--- post.py
+++ post.py
@@ -2,6 +2,6 @@ import requests
 
 result = requests.post('http://127.0.0.1:8000/api/save_entries/',
     json=[{"title": "Cars", "entries": [{"title": "21214"}]}])
-
+ # предварительно создай папку web на рабобочем столе
 
 print(result)
